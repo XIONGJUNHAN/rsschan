5274f94d9b3683f00eb1c0b08f3ef03a1311243d
XIONGJUNHAN <Xiongluojian@gmail.com>
2022-02-18 16:42:25 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -58,7 +58,7 @@ push_config = {
 
     'IGOT_PUSH_KEY': '',                # iGot 聚合推送的 IGOT_PUSH_KEY
 
-    'PUSH_KEY': 'SCT121927Tb6JS1ZWEVbU325ze2yDYGpXa',                     # server 酱的 PUSH_KEY，兼容旧版与 Turbo 版
+    'PUSH_KEY': '',                     # server 酱的 PUSH_KEY，兼容旧版与 Turbo 版
 
     'PUSH_PLUS_TOKEN': '',              # push+ 微信推送的用户令牌
     'PUSH_PLUS_USER': '',               # push+ 微信推送的群组编码
