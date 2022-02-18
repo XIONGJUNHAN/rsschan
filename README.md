55a23a039bab6be1bb698432a6c7943a97656c64
n0raml <61773298+n0raml@users.noreply.github.com>
2022-02-18 13:12:41 +0800

    创建文件

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# rsschan
- A update&push rss/feed tool
+# RSS Chan
+
+> RSS update&notification(Bark、DingTalk、FeiShu、go-cqhttp、gotify、iGot、server chan、push+、qmsg chan、WeChat work、telegram). - RSS更新与通知(Bark、钉钉群机器人、飞书群机器人、go-cqhttp、gotify、iGot、server chan、push+、qmsg chan、WeChat work、telegram)。
+
+# 功能
+RSS酱 运行在github action上的rss自动更新通知项目
+- 支持多rss订阅源
+- 支持多种推送通知
+- 支持Github Action
+- 过滤重复消息
+- ~~精准定时(云函数实现)待实现~~
+
+# 目录文件
+- main.py        主程序
+- notify.py      通知库
+- oldrss         rss记录
+- rss_sub        rss订阅
+
+# 使用方法
+1. Fork本项目
+2. 项目添加必须变量 Settings -> Secrets -> Actions -> New repository secret
+    - USERNAME           Github用户名
+    - USEREMAIL          Github邮箱
+    - 其他push token变量名请看notify.py下的push_config名字
+3. 添加订阅至 rss_sub
+4. 在action里启动或者 右上角 Star 启动并查看log
---
+++ main.py
@@ -0,0 +1,40 @@
+import feedparser
+import re
+import notify
+
+#删除多余html标签和超过2048字节数的字
+def delhtml(t):
+    pattern = re.compile(r'<[^>]+>',re.S)
+    nohtml = pattern.sub('', t)
+
+    #最大微信推送可传输字节数2048 太多信息导致信息流太长 一个汉字占2字节
+    if len(nohtml) > 256:
+        return '\n文章过长请查看原文'
+    else:
+        return nohtml
+
+#获取最新内容
+def GetNewRSS(url):
+    f=feedparser.parse(url)
+    for post in f.entries:
+        oldrss=open('oldrss',mode='a+')
+
+        #读取之前的rss
+        with open("oldrss") as file:
+            old = file.read()
+
+        #检查文章链接是否存在如果不存在则发送
+        if not post.link in old:
+            #打印文章标题
+            print(f.feed.title,post.title)
+            notify.send('<a href="'+post.link+'">'+f.feed.title+' - '+post.title+'</a>\n'+post.published,delhtml(post.description))
+            oldrss.writelines([f.feed.title,'  ',post.link,'  ',post.title,'\n'])
+
+        oldrss.close()
+
+if __name__ == '__main__':
+    #订阅地址在rss_sub文件，每行填一个网址。
+
+    #读取rss_sub文件，获取订阅地址。并逐行订阅
+    for line in open("rss_sub"):
+        GetNewRSS(line)
